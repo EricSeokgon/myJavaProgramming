7a3ba84889b0984ba1df8bbb840673a0775ba0a1
EricSeokgon <leesk55@gmail.com>
2022-01-04 16:55:30 +0900

    자료형 보다 큰 값 에러

---
+++ src/chapter03/Ex02_TypeChange2.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class Ex02_TypeChange2 {
+    public static void main(String[] args) {
+        int num1 = 2147483647;
+        //int num2 = 2147483648;
+        //long num3 = 2147483648;
+        long num4 = 2147483648L;
+
+        //float num5 = 1.0;
+        float num6 = 1.0F;
+
+        double num7 = 30;
+
+    }
+}

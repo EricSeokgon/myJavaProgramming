6dcbb20c60e5c636e309c36c6329ce0a835cf268
EricSeokgon <leesk55@gmail.com>
2022-01-04 16:52:49 +0900

    자료형 변환 에러 예제

--- src/chapter03/Ex02_TypeChange1.java
+++ src/chapter03/Ex02_TypeChange1.java
@@ -5,11 +5,11 @@ public class Ex02_TypeChange1 {
         int num1 = 1;
         byte num2 = 1;
         byte num3 = 127;
-        byte num4 = 128;
+        //byte num4 = 128;
 
         short num5 = 1;
 
-        num2 = num5;
+        //num2 = num5;
         num2 = (byte) num5;
     }
 }

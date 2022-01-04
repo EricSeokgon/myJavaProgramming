b3ca7f8a10c5b03d73be210eef6ad3a795b69830
EricSeokgon <leesk55@gmail.com>
2022-01-04 17:00:14 +0900

    연산시 형변환 에러

---
+++ src/chapter03/Ex04_Calculation1.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class Ex04_Calculation1 {
+    public static void main(String[] args) {
+        byte num11 = 1;
+        byte num12 = 2;
+        //byte result1 = num11 + num12;
+
+        short num21 = 1;
+        short num22 = 2;
+        //short result2 = num21 + num22;
+
+        short result22 = (short) (num21 + num22); //강제 형변환
+
+    }
+}

f5805ff9d1173d950e09bc194658b3adb01eb8e2
EricSeokgon <leesk55@gmail.com>
2022-01-04 16:28:59 +0900

    논리 자료형 예제

---
+++ src/chapter02/Ex03_boolean.java
@@ -0,0 +1,14 @@
+package chapter02;
+
+public class Ex03_boolean {
+    public static void main(String[] args) {
+        boolean check1 = true; //직접 값 할당
+        boolean check2 = false;
+        boolean check3 = (1 < 2); //연산의 결과를 값으로 할당
+
+        System.out.println(check1);
+        System.out.println(check2);
+        System.out.println(check3);
+        System.out.println(1 > 2); //연산 결과 출력
+    }
+}

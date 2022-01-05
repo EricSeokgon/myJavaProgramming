91ec998e903472f3fe6011154d4929d909382b72
EricSeokgon <leesk55@gmail.com>
2022-01-05 13:42:38 +0900

    연산자 우선순위 사용법

---
+++ src/chapter04/Ex09_Order.java
@@ -0,0 +1,15 @@
+package chapter04;
+
+public class Ex09_Order {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("\n");
+        System.out.println("-----------------");
+
+        int num = 5;
+        System.out.println(num + "\n");
+        System.out.println("-----------------");
+        System.out.println(num + "\n");
+        System.out.println("3 >=2 : " + (3 >= 2));
+    }
+}

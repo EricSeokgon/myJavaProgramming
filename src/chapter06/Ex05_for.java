7f9c9ddffbc33457580987048f6fb850390a0090
EricSeokgon <leesk55@gmail.com>
2022-01-07 11:39:51 +0900

    for문을 이용한 반복

---
+++ src/chapter06/Ex05_for.java
@@ -0,0 +1,21 @@
+package chapter06;
+
+public class Ex05_for {
+    public static void main(String[] args) {
+        for (int i = 2; i < 10; i++) {
+            System.out.print((2 * i) + " ");
+        }
+        System.out.println();
+
+        for (int i = 2; i < 10; i++)
+            System.out.print((3 * i) + " ");
+        System.out.println();
+
+        for (int i = 2; i < 10; i++) System.out.print((4 * i) + " ");
+        System.out.println();
+
+        for (int i = 2; i < 10; i++)
+            System.out.print((5 * i) + " ");
+        System.out.println();
+    }
+}

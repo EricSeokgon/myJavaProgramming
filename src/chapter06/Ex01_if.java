abd4781bb1a82141b9fc1883b7401720fb718978
EricSeokgon <leesk55@gmail.com>
2022-01-07 11:30:22 +0900

    if 조건문 사용법

---
+++ src/chapter06/Ex01_if.java
@@ -0,0 +1,18 @@
+package chapter06;
+
+public class Ex01_if {
+    public static void main(String[] args) {
+        int num = 100;
+        if (num > 50) {
+            System.out.println("num 변수의 길이 50보다 큽니다.");
+        }
+
+        if (num > 50)
+            System.out.println("num 변수의 길이 50보다 큽니다.");
+
+        if (num > 50) System.out.println("num 변수의 길이 50보다 큽니다.");
+
+        if (num < 50)
+            System.out.println(num);
+    }
+}

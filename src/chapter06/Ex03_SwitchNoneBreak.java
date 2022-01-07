0652a50cc3657930fb09d0441bb36ef3220e9c03
EricSeokgon <leesk55@gmail.com>
2022-01-07 11:34:54 +0900

    switch 기본 문법 break문 생략

---
+++ src/chapter06/Ex03_SwitchNoneBreak.java
@@ -0,0 +1,16 @@
+package chapter06;
+
+public class Ex03_SwitchNoneBreak {
+    public static void main(String[] args) {
+        int n = 6;
+        switch (n % 7) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            default:
+                System.out.println("주말");
+        }
+    }
+}

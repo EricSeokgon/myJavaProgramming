3c8e73cbbc69de9fd2defb039a619700ed89d289
EricSeokgon <leesk55@gmail.com>
2022-01-18 18:02:24 +0900

    문자열에서 문자 찾기 indexOf()

---
+++ src/chapter14/Ex05_indexOf.java
@@ -0,0 +1,13 @@
+package chapter14;
+
+public class Ex05_indexOf {
+    public static void main(String[] args) {
+        String str = "AppleBananaOrange";
+        int num1 = str.indexOf("a");
+        int num2 = str.indexOf("a", num1 + 1);
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+    }
+}

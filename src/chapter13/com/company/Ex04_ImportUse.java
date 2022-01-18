669fca71daac9dc2b21572c9c20c3f62c15d5167
EricSeokgon <leesk55@gmail.com>
2022-01-18 16:32:26 +0900

    임포트 사용법

---
+++ src/chapter13/com/company/Ex04_ImportUse.java
@@ -0,0 +1,15 @@
+package chapter13.com.company;
+
+
+public class Ex04_ImportUse {
+    public static void main(String[] args) {
+        Banana banana = new Banana();
+        banana.showName();
+    }
+
+    private static class Banana {
+        public void showName() {
+        }
+    }
+
+}

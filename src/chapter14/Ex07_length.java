5b43c27ec179c6c28c43f76055edd337621fe711
EricSeokgon <leesk55@gmail.com>
2022-01-19 12:39:05 +0900

    문자열 길이 구하기 length()

--- src/chapter14/Ex07_length.java
+++ src/chapter14/Ex07_length.java
@@ -3,7 +3,7 @@ package chapter14;
 public class Ex07_length {
     public static void main(String[] args) {
         String str = "apple";
-        for (int i = 10; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             if (ch == 'l') {
                 break;

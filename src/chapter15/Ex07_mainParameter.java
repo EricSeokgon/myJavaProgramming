7ea63a2a3b2bb5d01081a10fef2dd385bc12cbfb
EricSeokgon <leesk55@gmail.com>
2022-01-20 18:02:37 +0900

    main() 메서드의 매개변수

---
+++ src/chapter15/Ex07_mainParameter.java
@@ -0,0 +1,9 @@
+package chapter15;
+
+public class Ex07_mainParameter {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}

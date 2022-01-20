58b3266c70294f5dfc536a985ba54c607792747e
EricSeokgon <leesk55@gmail.com>
2022-01-20 18:04:49 +0900

    for-each문

---
+++ src/chapter15/Ex08_EnhancedFor.java
@@ -0,0 +1,17 @@
+package chapter15;
+
+public class Ex08_EnhancedFor {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int e : arr) {
+            System.out.println(e + " ");
+        }
+        System.out.println();
+
+        int sum = 0;
+        for (int e : arr) {
+            sum = sum + e;
+        }
+        System.out.println("sum :" + sum);
+    }
+}

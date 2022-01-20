bc5f7461fe8f2558dc273aa9279bc485bb3aa236
EricSeokgon <leesk55@gmail.com>
2022-01-20 15:21:34 +0900

    배열 사용 : 클래스형

---
+++ src/chapter15/Ex04_BoxArray.java
@@ -0,0 +1,25 @@
+package chapter15;
+
+import javax.swing.*;
+
+class BoxA4 {
+    public String name;
+
+    BoxA4(String name) {
+        this.name = name;
+    }
+}
+
+public class Ex04_BoxArray {
+    public static void main(String[] args) {
+        BoxA4[] ar = new BoxA4[3];
+
+        ar[0] = new BoxA4("홍길동");
+        ar[1] = new BoxA4("전우치");
+        ar[2] = new BoxA4("손오공");
+
+        System.out.println(ar[0].name);
+        System.out.println(ar[1].name);
+        System.out.println(ar[2].name);
+    }
+}

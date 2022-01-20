8ba802424b1f10a865193b864901c593634ecf24
EricSeokgon <leesk55@gmail.com>
2022-01-20 18:05:20 +0900

    for-each문

--- src/chapter15/Ex08_EnhancedFor.java
+++ src/chapter15/Ex08_EnhancedFor.java
@@ -4,7 +4,7 @@ public class Ex08_EnhancedFor {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         for (int e : arr) {
-            System.out.println(e + " ");
+            System.out.print(e + " ");
         }
         System.out.println();
 

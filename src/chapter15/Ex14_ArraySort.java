dd149e07dd9b06ddc68a7f3904ab991dc1b60129
EricSeokgon <leesk55@gmail.com>
2022-01-21 16:53:17 +0900

    배열의 내용 정렬

---
+++ src/chapter15/Ex14_ArraySort.java
@@ -0,0 +1,30 @@
+package chapter15;
+
+import java.util.Arrays;
+
+public class Ex14_ArraySort {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 2, 4};
+        double[] arr2 = {4.4, 3.3, 2.2, 1.1};
+        String[] arr3 = {"홍길동", "전우치", "손오공", "멀린"};
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        Arrays.sort(arr3);
+
+        for (int n : arr1) {
+            System.out.print(n + "tn");
+        }
+        System.out.println();
+
+        for (double d : arr2) {
+            System.out.print(d + "tn");
+        }
+        System.out.println();
+
+        for (String s : arr3) {
+            System.out.print(s + "tn");
+        }
+        System.out.println();
+    }
+}

0bc10d60097fa743062f581f3c7a7c1c5f396720
EricSeokgon <leesk55@gmail.com>
2022-01-21 17:04:34 +0900

    예외 상황 만들기

---
+++ src/chapter16/Ex01_ExceptionCase.java
@@ -0,0 +1,16 @@
+package chapter16;
+
+import java.util.Scanner;
+
+public class Ex01_ExceptionCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = 10 / num1;
+
+        System.out.println(num2);
+
+        //MyBook book1 = new MyBook();
+
+    }
+}

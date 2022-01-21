b4b2426a5cab7d69660405b6af4d73006a08656b
EricSeokgon <leesk55@gmail.com>
2022-01-21 16:49:46 +0900

    배열의 내용 비교

---
+++ src/chapter15/Ex13_ArrayEquals.java
@@ -0,0 +1,13 @@
+package chapter15;
+
+import java.util.Arrays;
+
+public class Ex13_ArrayEquals {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = Arrays.copyOf(arr1, arr1.length);
+
+        boolean bCheck = Arrays.equals(arr1, arr2);
+        System.out.println(bCheck);
+    }
+}

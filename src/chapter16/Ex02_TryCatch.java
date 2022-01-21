08d1287eda9c1e21e6b982174e4ea69e6e45a949
EricSeokgon <leesk55@gmail.com>
2022-01-21 17:09:34 +0900

    try~catch

---
+++ src/chapter16/Ex02_TryCatch.java
@@ -0,0 +1,26 @@
+package chapter16;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ex02_TryCatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int num1 = sc.nextInt();
+            int num2 = 10 / num1;
+            System.out.println(num2);
+            System.out.println("Good bye~~~~");
+        } catch (ArithmeticException e) {
+            String str = e.getMessage();
+            System.out.println(str);
+            if (str.equals("/ by zero")) {
+                System.out.println("0으로 나눌 수 없습니다.");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println(e.getMessage());
+        //    e.printStackTrace();
+        }
+
+    }
+}

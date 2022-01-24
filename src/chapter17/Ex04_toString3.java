685191a1de7e6c17422184774615cd3c6c521562
EricSeokgon <leesk55@gmail.com>
2022-01-24 16:34:28 +0900

    toString() 메서드3

---
+++ src/chapter17/Ex04_toString3.java
@@ -0,0 +1,18 @@
+package chapter17;
+
+class Book4 {
+    String author;
+
+    public String getAuthor() { //메서드 오버라이딩
+        return author;
+    }
+}
+
+public class Ex04_toString3 {
+    public static void main(String[] args) {
+        Book3 myBook = new Book3();
+        myBook.author = "홍길동";
+        System.out.println(myBook.author); //참조 변수의 멤버 변수의 값 출력
+        System.out.println(myBook); //객체 자체 출력
+    }
+}

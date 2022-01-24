6afeb6dc4eb80345f5a97102c95d3e4baf096e4c
EricSeokgon <leesk55@gmail.com>
2022-01-24 16:38:33 +0900

    equals() 메서드

--- src/chapter17/Ex04_toString3.java
+++ src/chapter17/Ex04_toString3.java
@@ -10,7 +10,7 @@ class Book4 {
 
 public class Ex04_toString3 {
     public static void main(String[] args) {
-        Book3 myBook = new Book3();
+        Book4 myBook = new Book4();
         myBook.author = "홍길동";
         System.out.println(myBook.author); //참조 변수의 멤버 변수의 값 출력
         System.out.println(myBook); //객체 자체 출력

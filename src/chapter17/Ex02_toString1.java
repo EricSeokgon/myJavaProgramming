a22f3b8bfd1a8f5ecb7732a56d08e350bf3881f3
EricSeokgon <leesk55@gmail.com>
2022-01-24 16:31:14 +0900

    toString() 메서드

---
+++ src/chapter17/Ex02_toString1.java
@@ -0,0 +1,12 @@
+package chapter17;
+
+import java.util.Locale;
+
+public class Ex02_toString1 {
+    public static void main(String[] args) {
+        String name = "홍길동";
+        System.out.println(name);
+        System.out.println(name.toString());
+
+    }
+}

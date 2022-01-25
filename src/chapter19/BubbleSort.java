01961de2437f340dd7dac18afe0765d00f89d5a5
EricSeokgon <leesk55@gmail.com>
2022-01-25 17:06:05 +0900

    입력된 값 정렬하기

--- src/chapter19/BubbleSort.java
+++ src/chapter19/BubbleSort.java
@@ -12,6 +12,18 @@ public class BubbleSort {
         }
     }
 
+    public static void bubbleSort(int[] num) {
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num.length; j++) {
+                if (num[j] > num[j + 1]) {
+                    int tmp = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         int[] num = new int[10];

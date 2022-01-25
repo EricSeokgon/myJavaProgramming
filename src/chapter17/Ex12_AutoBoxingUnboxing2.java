94dce259770b9384d4874d95079dd2817dd5d238
EricSeokgon <leesk55@gmail.com>
2022-01-25 14:01:32 +0900

    오토 박싱과 오토 언박싱2

---
+++ src/chapter17/Ex12_AutoBoxingUnboxing2.java
@@ -0,0 +1,19 @@
+package chapter17;
+
+public class Ex12_AutoBoxingUnboxing2 {
+    public static void main(String[] args) {
+        Integer num = 10;
+        num++;
+
+        System.out.println(num);
+
+        num += 3;
+        System.out.println(num);
+
+        int r = num + 5;
+        Integer rObj = num - 5;
+
+        System.out.println(r);
+        System.out.println(rObj);
+    }
+}

b63a59e63e135d94a24848fafccc4608a563ea3c
EricSeokgon <leesk55@gmail.com>
2022-01-25 14:03:54 +0900

    오토 박싱과 오토 언박싱2

--- src/chapter17/Ex12_AutoBoxingUnboxing2.java
+++ src/chapter17/Ex12_AutoBoxingUnboxing2.java
@@ -3,11 +3,12 @@ package chapter17;
 public class Ex12_AutoBoxingUnboxing2 {
     public static void main(String[] args) {
         Integer num = 10;
-        num++;
+
+        num++; //Integer.valueOf(num.intValue()+1)
 
         System.out.println(num);
 
-        num += 3;
+        num += 3; //Integer.valueOf(num.intValue()+3)
         System.out.println(num);
 
         int r = num + 5;

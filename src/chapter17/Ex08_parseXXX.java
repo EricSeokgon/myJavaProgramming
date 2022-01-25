83636f826adb8330c544479bf81159d105ca90e6
EricSeokgon <leesk55@gmail.com>
2022-01-25 13:50:26 +0900

    문자열 변환

---
+++ src/chapter17/Ex08_parseXXX.java
@@ -0,0 +1,11 @@
+package chapter17;
+
+public class Ex08_parseXXX {
+    public static void main(String[] args) {
+        String str = "100";
+        int a = Integer.parseInt(str);
+        double b = Double.parseDouble("3.14");
+
+        System.out.println(a + " : " + b);
+    }
+}

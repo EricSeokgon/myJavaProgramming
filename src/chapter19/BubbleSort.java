d4179e16365fbbf85b4b98baa7bc571877bb1b21
EricSeokgon <leesk55@gmail.com>
2022-01-25 17:03:39 +0900

    입력처리 코드를 메서드로 분리하기

--- src/chapter19/BubbleSort.java
+++ src/chapter19/BubbleSort.java
@@ -3,13 +3,18 @@ package chapter19;
 import java.util.Scanner;
 
 public class BubbleSort {
-    public static void main(String[] args) {
+    public static void getNumber(int[] num) {
         Scanner sc = new Scanner(System.in);
-        int[] num = new int[10];
+        System.out.println("10개의 정수를 무작위로 입력하세요.");
 
         for (int i = 0; i < num.length; i++) {
             num[i] = sc.nextInt();
         }
+    }
+
+    public static void main(String[] args) {
+
+        int[] num = new int[10];
 
         for (int i = 0; i < num.length; i++) {
             System.out.print(num[i] + " ");

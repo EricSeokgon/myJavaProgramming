adbfd7ff7de7348c1d832892be0ebbff5cbf1d14
EricSeokgon <leesk55@gmail.com>
2022-01-25 13:47:29 +0900

    Number 클래스

---
+++ src/chapter17/Ex07_Number.java
@@ -0,0 +1,15 @@
+package chapter17;
+
+public class Ex07_Number {
+    public static void main(String[] args) {
+        Integer num1 = Integer.valueOf(20);
+        System.out.println(num1.intValue());
+        System.out.println(num1.doubleValue());
+
+        Double num2 = Double.valueOf(3.14);
+        System.out.println(num2.intValue());
+        System.out.println(num2.doubleValue());
+    }
+
+
+}

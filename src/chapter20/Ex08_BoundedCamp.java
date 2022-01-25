6df89fbad969aa134e7fbef405231b5a7fd544a3
EricSeokgon <leesk55@gmail.com>
2022-01-25 17:53:25 +0900

    제네릭 클래스의 매개변수 타입 제한하기

---
+++ src/chapter20/Ex08_BoundedCamp.java
@@ -0,0 +1,19 @@
+package chapter20;
+
+class Camp8<T extends Number> {
+    private T ob;
+
+    public void set(T o) {
+        ob = o;
+    }
+
+    public T get() {
+        return ob;
+    }
+}
+
+public class Ex08_BoundedCamp {
+    public static void main(String[] args) {
+
+    }
+}

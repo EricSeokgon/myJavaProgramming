911cf8a50fe862cdd565945a791e5beb266389cc
EricSeokgon <leesk55@gmail.com>
2022-01-26 17:31:35 +0900

    Set<E> 인터페이스를 구현하는 컬렉션 클래스들 HashSet 중복비교

---
+++ src/chapter21/Ex07_HashSetEqual.java
@@ -0,0 +1,51 @@
+package chapter21;
+
+import java.util.HashSet;
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String toString() {
+        return name + " : " + age;
+    }
+
+
+    public int hashCode() {
+        int num = age % 3;
+        System.out.println(num);
+        return num;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("비교를 합니다.");
+        if (age == ((Student) obj).age) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+public class Ex07_HashSetEqual {
+    public static void main(String[] args) {
+        HashSet<Student> set = new HashSet<>();
+        set.add(new Student("홍길동", 20));
+        set.add(new Student("전우치", 20));
+        set.add(new Student("홍길동", 25));
+
+        System.out.println("객체 수 :" + set.size());
+
+        for (Student s : set) {
+            System.out.print(s.toString() + "\t");
+        }
+        System.out.println();
+    }
+}

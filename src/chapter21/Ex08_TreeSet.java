a9c9b7a45cbed91e30a21aaa59dbf32707e29598
EricSeokgon <leesk55@gmail.com>
2022-01-26 17:38:01 +0900

    TreeSet 사용하기

---
+++ src/chapter21/Ex08_TreeSet.java
@@ -0,0 +1,23 @@
+package chapter21;
+
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.TreeSet;
+
+public class Ex08_TreeSet {
+    public static void main(String[] args) {
+        TreeSet<String> tree = new TreeSet<>();
+        tree.add("홍길동");
+        tree.add("전우치");
+        tree.add("손오공");
+        tree.add("멀린");
+        tree.add("손오공");
+
+        System.out.println("객체 수:" + tree.size());
+
+        for (Iterator<String> itr = tree.iterator(); itr.hasNext(); ) {
+            System.out.print(itr.next().toString() + "\t");
+        }
+        System.out.println();
+    }
+}

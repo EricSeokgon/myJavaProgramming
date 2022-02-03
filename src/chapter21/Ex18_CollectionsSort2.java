6ce5ba5d065c367d9218d6508c73641c512d300c
EricSeokgon <leesk55@gmail.com>
2022-02-03 14:10:21 +0900

    정렬 내림차순

---
+++ src/chapter21/Ex18_CollectionsSort2.java
@@ -0,0 +1,30 @@
+package chapter21;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+class StringDesc implements Comparator<String> {
+    public int compare(String s1, String s2) {
+        return s2.compareTo(s1);
+    }
+}
+
+public class Ex18_CollectionsSort2 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("홍길동");
+        list.add("전우치");
+        list.add("손오공");
+
+        //정렬 :  오름차순
+        Collections.sort(list);
+        System.out.println(list);
+
+        StringDesc cmp = new StringDesc();
+
+        //정렬 : 내림차순
+        Collections.sort(list, cmp);
+        System.out.println(list);
+    }
+}

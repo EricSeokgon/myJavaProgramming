50a3837d34510de5d740b4cadb4c71e4cb3bffdb
EricSeokgon <leesk55@gmail.com>
2022-02-03 15:31:58 +0900

    익명 내부 클래스 람다식2

---
+++ src/chapter22/Ex02_Lambda2.java
@@ -0,0 +1,18 @@
+package chapter22;
+
+interface Unit6 {
+    void move(String s);
+}
+
+
+public class Ex02_Lambda2 {
+    public static void main(String[] args) {
+        Unit6 unit = new Unit6() {
+
+            public void move(String s) {
+                System.out.println(s);
+            }
+        };
+        unit.move("anonymous : Unit 6");
+    }
+}

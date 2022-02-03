9b7665d3dec42494b48f8584c71b26b36d00305b
EricSeokgon <leesk55@gmail.com>
2022-02-03 14:56:43 +0900

    멤버 내부 클래스

---
+++ src/chapter22/Ex01_MemberInner.java
@@ -0,0 +1,32 @@
+package chapter22;
+
+class Outer1 {
+    private int speed = 10;
+
+    class MemberInner1 {
+
+        public void move() {
+            System.out.printf("인간형 유닛이 %d 속도로 이동합니다.\n", speed);
+        }
+    }
+
+    public void getMarine() {
+        MemberInner1 inner = new MemberInner1();
+        inner.move();
+    }
+}
+
+public class Ex01_MemberInner {
+    public static void main(String[] args) {
+        Outer1 out = new Outer1();
+
+        //out 기반으로 생성된 객체의 메서드 호출
+        out.getMarine();
+
+        //out 기반으로 내부 클래스 객체 생성
+        Outer1.MemberInner1 inner = out.new MemberInner1();
+
+        //inner 기반으로 생성된 객체의 메서드 호출
+        inner.move();
+    }
+}

e2f5379155850d398ce6b5959db5a7db7115a78a
EricSeokgon <leesk55@gmail.com>
2022-02-03 17:28:09 +0900

    여러가지 연산들 sorted()

---
+++ src/chapter23/Ex04_Sorted.java
@@ -0,0 +1,23 @@
+package chapter23;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex04_Sorted {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("홍길동", "멀린", "해리포터");
+
+        //사전순 정렬
+        list.stream()
+                .sorted()
+                .forEach(n -> System.out.print(n + "\t"));
+        System.out.println();
+
+        //글자 길이순 정렬
+        list.stream()
+                .sorted((s1, s2) -> s1.length() - s2.length())
+                .forEach(n -> System.out.print(n + "\t"));
+        System.out.println();
+
+    }
+}

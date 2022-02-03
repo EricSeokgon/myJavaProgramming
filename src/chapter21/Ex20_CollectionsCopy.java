6c8040ae8abe09902cc323f02dbbb2e7f339d78a
EricSeokgon <leesk55@gmail.com>
2022-02-03 14:16:41 +0900

    복사

---
+++ src/chapter21/Ex20_CollectionsCopy.java
@@ -0,0 +1,29 @@
+package chapter21;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Ex20_CollectionsCopy {
+    public static void main(String[] args) {
+        List<String> src = Arrays.asList("홍길동", "전우치", "손오공", "멀린");
+
+        //수정 가능한 리스트 생성
+        List<String> dst = new ArrayList<>(src);
+        System.out.println(dst);
+
+        //정렬하여 그 결과를 출력
+        Collections.sort(dst);
+        System.out.println(dst);
+
+        //사정상 정렬 이전의 상태로 되돌려야 함
+        Collections.copy(dst, src);
+        System.out.println(dst);
+
+        //수정 가능한지 확인
+        dst.remove(0);
+        System.out.println(dst);
+    }
+
+}

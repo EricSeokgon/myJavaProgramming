a064dbb9a52715b83ff45fb20d88885ef04a3d04
EricSeokgon <leesk55@gmail.com>
2022-02-04 13:07:13 +0900

    입출력 스트림 예외 직접 처리

---
+++ src/chapter24/Ex02_FileWrite2.java
@@ -0,0 +1,28 @@
+package chapter24;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Ex02_FileWrite2 {
+    public static void main(String[] args) {
+        OutputStream out = null;
+
+        try {
+            out = new FileOutputStream("data.txt")
+            out.write(65);
+        } catch (IOException e) {
+
+        }finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+
+                }
+
+            }
+        }
+
+    }
+}

aa58558861691e3a914a61ef701a5cf19861ffc2
EricSeokgon <leesk55@gmail.com>
2022-02-07 17:41:57 +0900

    스레드에서의 문제점

---
+++ src/chapter25/Ex06_ProblemOfThread.java
@@ -0,0 +1,32 @@
+package chapter25;
+
+public class Ex06_ProblemOfThread {
+    public static int money = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task1 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                money++;
+            }
+        };
+
+        Runnable task2 = () -> {
+            for (int i = 0; i < 10000; i++) {
+                money--;
+            }
+        };
+
+        Thread t1 = new Thread(task1);
+        Thread t2 = new Thread(task2);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(money);
+
+    }
+
+}
